b61f13444755190abb7c46120058f9d187077ad7
hezhiqiang <simlesos@gmail.com>
2017-08-08 16:13:04 +0800

    fix: webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,9 +5,8 @@ import LodashModuleReplacementPlugin from 'lodash-webpack-plugin'
 export default {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname),
-    filename: 'index.js',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'YCWebViewBridge.js'
   },
   module: {
     rules: [{
